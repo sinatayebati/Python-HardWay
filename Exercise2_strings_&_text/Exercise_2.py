31de123a09f4cd831bbc40bc3d4d28b00dbf7315
sina-tyi <sinatayebati@gmail.com>
2023-04-20 23:26:22 -0500

    exercise 2 update

--- Exercise2_strings_&_text/Exercise_2.py
+++ Exercise2_strings_&_text/Exercise_2.py
@@ -1,3 +1,5 @@
+# exercise strings and text
+
 types_of_people = 10
 x = f"there are {types_of_people} types of people"
 
@@ -13,3 +15,39 @@ print(f"I also said: '{y}'")
 
 hilarious = False
 joke_evaluation = "Isn't that joke so funny?! {}"
+
+# exercise more variables and text
+# practice "escape singe/double quotes"
+# here's some new strange stuff
+
+days = "Mond Tue Wed Thu Fri Sat Sun"
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+print("Here are the days: ", days)
+print("Here are the months: ", months)
+
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6!
+""")
+
+# another way to escpe single/double quotes
+# is to use scape sequences inside the quotes, like bellow
+
+tabby_cat = "\tI'm tabbed in."
+persian_cat = "I'm split\non a line"
+backslash_cat = "I'm \\ a \\ cat."
+
+fat_cat = """
+I'll do a list:
+\t* Cat food
+\t* Fishes
+\t* Catnip\n\t* Grass
+"""
+
+print(tabby_cat)
+print(persian_cat)
+print(backslash_cat)
+print(fat_cat)
